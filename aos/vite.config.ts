e35e216fcb6a95bd0c20e65e89a72910258e3999
Staijn1 <stein@jnkr.eu>
2024-06-02 21:13:39 +0200

    Vite works

--- aos/vite.config.ts
+++ aos/vite.config.ts
@@ -1,11 +1,39 @@
-import {defineConfig} from 'vite'
-import {resolve} from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import postcss from 'rollup-plugin-postcss'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+
+// Define the plugins
+const plugins = [
+  postcss({
+    extract: 'dist/aos.css',
+    plugins: [autoprefixer, cssnano]
+  }),
+  babel({
+    exclude: ['node_modules/**']
+  }),
+  terser()
+]
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: 'src/js/aos.js',
+      output: {
+        file: 'dist/aos.js',
+        format: 'umd',
+        sourcemap: process.env.NODE_ENV === 'dev'
+      },
+      plugins: plugins
+    }
+  },
   resolve: {
     alias: {
       src: resolve('src/')
     }
-  },
+  }
 })
